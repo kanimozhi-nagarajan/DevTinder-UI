c651c54527f69ee2c5ed26adffd99d512e917b0f
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2025-09-02 15:07:52 +0530

    Feat: created appStore,userSlice , installed React-redux , axios

---
+++ src/components/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+import axios from 'axios'
+
+import { useDispatch } from 'react-redux'
+
+import { addUser } from '../utils/userSlice'
+
+import { useNavigate } from 'react-router-dom'
+
+import { BASE_URL } from '../utils/constant'
+const Login = () => {
+
+const [email,setEmail]= useState('eleven@gmail.com')
+const [password,setPassword]= useState('Eleven@12345') 
+
+const dispatch = useDispatch()
+const navigate = useNavigate()
+
+const handleLogin = async () => {
+try{
+   const res = await axios.post(BASE_URL+"/login",
+    {
+      email,
+      password
+    },{withCredentials:true})
+        // console.log(res.data.data)
+
+    dispatch(addUser(res.data.data))
+    return navigate("/feed")
+}
+catch(err){
+  console.error(err)
+}
+}
+  
+  return (
+    <div className = "flex justify-center items-center my-20">
+    <div className="card bg-base-300 w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title justify-center">Login</h2>
+     <div>
+       <fieldset className="fieldset">
+           <legend className="fieldset-legend">Email Id </legend>
+              <input type="text" value={email} className="input" onChange={(e)=>setEmail(e.target.value)} />
+       </fieldset> 
+              <fieldset className="fieldset">
+           <legend className="fieldset-legend">Password </legend>
+              <input type="text" value={password} className="input" onChange={(e)=>setPassword(e.target.value)} />
+       </fieldset> 
+</div>
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default Login
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const NavBar = () => {
+
+  const user = useSelector(store=>store.user)
   return (
          <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">Dev Tinder</a>
   </div>
+  {user && (
+  
   <div className="flex gap-2">
+    <p className="form-control">Welcome ! {user.firstName}</p>
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-4">
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            alt="photoUrl"
+            src= {user.photoURL} />
         </div>
       </div>
       <ul
@@ -29,6 +35,7 @@ const NavBar = () => {
       </ul>
     </div>
   </div>
+  ) }
 </div>
   )
 }
