6148e3ee8ccf472798b01bd713383c79eda69959
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2025-09-02 10:46:29 +0530

    Feat: Created components , included routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 function App() {
-
-  return (
+  return (    
     <>
-      <h1>Vite + React</h1>
+           <BrowserRouter basename="/">
+           <Routes>
+            <Route path="/" element={<Body/>} >
+            <Route path="/login" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
+            </Route>
+           </Routes>
+           </BrowserRouter>  
     </>
+
   )
 }
 
